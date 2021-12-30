b84abc1cee08fa2eafbc67bc698315571cbad702
yongwoo-jeong <jywoo9@gmail.com>
2021-12-30 18:52:20 +0900

    useEffect and destroy call back

--- src/App.js
+++ src/App.js
@@ -1,33 +1,20 @@
 import { useState, useEffect } from "react";
 
-function App() {
-  const [counter, setValue] = useState(0);
-  const [keyword, setKeyword] = useState("");
-  const onClick = () => setValue((prev) => prev + 1);
-  const onChage = (event) => setKeyword(event.target.value);
+function Hello() {
   useEffect(() => {
-    console.log("I run only once");
+    console.log("CREATED 😋");
+    return () => console.log("DESTROYED 😪");
   }, []);
-  useEffect(() => {
-    console.log("I run when 'keyword' changes.");
-  }, [keyword]);
-  useEffect(() => {
-    console.log("I run when 'counter' changes.", keyword);
-  }, [counter]);
-  useEffect(() => {
-    console.log("I run when 'keyword & counter' changes.");
-  }, [keyword, counter]);
+  return <h1>Hello</h1>;
+}
 
+function App() {
+  const [showing, setShowing] = useState(false);
+  const onClick = () => setShowing((prev) => !prev);
   return (
     <div>
-      <input
-        value={keyword}
-        onChange={onChage}
-        type="text"
-        placeholder="Search here..."
-      />
-      <h1>{counter}</h1>
-      <button onClick={onClick}>Click me</button>
+      {showing ? <Hello /> : null}
+      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
     </div>
   );
 }
