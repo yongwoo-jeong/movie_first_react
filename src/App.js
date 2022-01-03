f30e0ad52b0bc512bebfe8400f0cc6f9a98aa79e
yongwoo-jeong <jywoo9@gmail.com>
2022-01-03 17:16:38 +0900

    only Screens Render  before Router

--- src/App.js
+++ src/App.js
@@ -1,57 +1,5 @@
-import { useEffect, useState } from "react";
-
 function App() {
-  const [loading, setLoading] = useState(true);
-  const [coins, setCoins] = useState([]);
-  const [targetCoin, setTargetCoin] = useState("");
-  const [balance, setBalance] = useState("");
-  const onDecide = (e) => setTargetCoin(e.target.value);
-  const handleInput = (e) => {
-    setBalance(e.target.value);
-  };
-  useEffect(() => {
-    fetch("https://api.coinpaprika.com/v1/tickers")
-      .then((response) => response.json())
-      .then((json) => {
-        setCoins(json);
-        setLoading(false);
-      });
-  }, []);
-  return (
-    <div>
-      <h1>The Coins! {loading ? "" : `(${coins.length})`}</h1>
-      <hr />
-      <hr />
-      {loading ? (
-        <strong>Loading...</strong>
-      ) : (
-        <select onChange={onDecide} value={targetCoin}>
-          <option>Select Coin First</option>
-          {coins.map((coin, index) => (
-            <option key={index} value={coin.quotes.USD.price}>
-              {coin.name} ({coin.symbol}): $
-              {Math.round((coin.quotes.USD.price * 100) / 100)} USD
-            </option>
-          ))}
-        </select>
-      )}
-      <div style={{ marginTop: "10px", marginRight: "10px" }}>
-        <input
-          onChange={handleInput}
-          value={balance}
-          placeholder="Write your Balance"
-        />
-        <strong>You can buy </strong>
-        {balance ? (
-          <h2>
-            You can convert TO {Math.round((balance / targetCoin) * 100) / 100}
-          </h2>
-        ) : (
-          <h3>Input your balance Please</h3>
-        )}
-      </div>
-    </div>
-  );
+  return null;
 }
 
 export default App;
